45ba6872ee77b60a27ab6f32257ca070b92215f5
Kate <kateto_stamatova@abv.bg>
2015-08-11 20:29:11 +0300

    Revert "updating the map"
    This reverts commit f0189728e1e81efe4f7e6753220eb58c45b6b99d.

--- src/Desktop/Map.java
+++ src/Desktop/Map.java
@@ -2,17 +2,9 @@ package Desktop;
 
 import java.awt.EventQueue;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-
 import java.awt.Color;
 import java.awt.Font;
-
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-
-import javax.swing.JLabel;
 
 public class Map {
 
@@ -48,13 +40,6 @@ public class Map {
 		frame = new JFrame();
 		frame.setAlwaysOnTop(true);
 		frame.getContentPane().setBackground(Color.WHITE);
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		
-		JLabel label = new JLabel(new ImageIcon("C:\\Users\\k8\\Downloads\\pirate.png"));
-		label.setIconTextGap(2);
-		panel.add(label);
 		frame.setBackground(Color.WHITE);
 		frame.setResizable(false);
 		frame.setBounds(100, 100, 1024, 740);
